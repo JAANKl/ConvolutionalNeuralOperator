higher resolution, improved normalising, fixed CNO, error vs attribute plots

.ipynb_checkpoints/Dataloaders-checkpoint.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
 
 from FNOModules import FNO2d
+from CNOModule import CNO
 from training.FourierFeatures import FourierFeatures
 
 from torch.utils.data import Dataset
@@ -42,7 +43,7 @@ def downsample(u, N):
 
 # For Straka Bubble:
 
-def resize_and_downsample(u, original_shape=(512, 128), target_shape=(128, 128)):
+def resize_and_downsample(u, original_shape=(512, 128), target_shape=(256, 256)):
     """
     Resize and downsample the input tensor to a new shape using bilinear interpolation.
     Args:
@@ -68,6 +69,14 @@ def resize_and_downsample(u, original_shape=(512, 128), target_shape=(128, 128))
     resized_u = resized_u.squeeze(0).squeeze(0)
     
     return resized_u
+
+
+def normalize_data(inputs, labels):
+    for i in [0,1,4]:
+        inputs[i, :, :] = (inputs[i, :, :] - inputs[i, :, :].min())/(inputs[i, :, :].max() - inputs[i, :, :].min())
+    labels = (labels - labels.min())/(labels.max() - labels.min())
+    
+    return inputs, labels
 
 #------------------------------------------------------------------------------
 
@@ -137,11 +146,23 @@ def default_param_CNO(network_properties):
 
 
 class StrakaBubbleDataset(Dataset):
-    def __init__(self, which, training_samples, model_type, dt):
+    def __init__(self, which, training_samples, model_type, dt, normalize=True):
         
         self.dt = dt
         self.max_time = 900
         self.model_type = model_type
+        self.normalize = normalize
+        self.min_data = -28.0
+        self.max_data = 0.0
+        self.min_x = 0.0
+        self.max_x = 25575.0
+        self.min_z = 0.0
+        self.max_z = 6400.0
+        self.min_viscosity = 0.029050784477663294
+        self.max_viscosity = 74.91834790806836
+        self.min_diffusivity = 0.043989764422611155
+        self.max_diffusivity = 74.8587964361266
+        self.resolution = 256
         #The file:
         
         self.file_data = "/cluster/work/math/camlab-data/data_dana/4-Straka_vdIC_uvDT_timeseries_1024samples/samples/data/sample_"
@@ -188,15 +209,23 @@ class StrakaBubbleDataset(Dataset):
             labels = torch.tensor(temp_values_t_out, dtype=torch.float32)
 
 
-            # Resize and downsample
-            inputs = torch.stack([resize_and_downsample(inputs[i, :, :], (512, 128), (128, 128)) for i in range(inputs.shape[0])])
-            labels = resize_and_downsample(labels.squeeze(0), (512, 128), (128, 128))
+            # Resize and downsample TODO: go to 256x256
+            inputs = torch.stack([resize_and_downsample(inputs[i, :, :], (512, 128), (self.resolution, self.resolution)) for i in range(inputs.shape[0])])
+            labels = resize_and_downsample(labels.squeeze(0), (512, 128), (self.resolution, self.resolution)) 
 
             # Normalising
-            inputs = (inputs - temp_values_t_in.min())/(temp_values_t_in.max() - temp_values_t_in.min())
-            # inputs = (inputs - temp_values_t0.mean())/temp_values_t0.std()
-            labels = (labels - temp_values_t_out.min())/(temp_values_t_out.max() - temp_values_t_out.min())
-            # labels = (labels - temp_values_t300.mean())/temp_values_t300.std()
+            if self.normalize:
+                
+                # inputs = (inputs - self.min_data)/(self.max_data - self.min_data)
+                # labels = (labels - self.min_data)/(self.max_data - self.min_data)
+                
+                
+                inputs[0, :, :] = (inputs[0, :, :] - self.min_x)/(self.max_x - self.min_x)
+                inputs[1, :, :] = (inputs[1, :, :] - self.min_z)/(self.max_z - self.min_z)
+                inputs[2, :, :] = (inputs[2, :, :] - self.min_viscosity)/(self.max_viscosity - self.min_viscosity)
+                inputs[3, :, :] = (inputs[3, :, :] - self.min_diffusivity)/(self.max_diffusivity - self.min_diffusivity)
+                inputs[4, :, :] = (inputs[4, :, :] - self.min_data)/(self.max_data - self.min_data)
+                labels = (labels - self.min_data)/(self.max_data - self.min_data)
 
             if self.model_type == "FNO":
                 inputs = inputs.permute(1, 2, 0)
@@ -205,16 +234,19 @@ class StrakaBubbleDataset(Dataset):
                 labels = labels.unsqueeze(0)
             else:
                 raise(NotImplementedError("Only FNO and CNO supported."))
+
             data_pairs.append((inputs, labels))
 
         return data_pairs
     
 class StrakaBubblePlottingDataset(Dataset):
-    def __init__(self, which, training_samples, model_type, t_in=0, t_out=900):
+    def __init__(self, which, training_samples, model_type, t_in=0, t_out=900, normalize=True):
         self.model_type = model_type
         self.t_in = t_in
         self.t_out = t_out
         self.max_time = 900
+        self.normalize = normalize
+        self.resolution = 256
         assert self.t_in in 60 * np.array(range(16))
         assert self.t_out in 60 * np.array(range(16))
         assert self.t_in < self.t_out
@@ -263,14 +295,21 @@ class StrakaBubblePlottingDataset(Dataset):
 
 
         # Resize and downsample
-        inputs = torch.stack([resize_and_downsample(inputs[i, :, :], (512, 128), (128, 128)) for i in range(inputs.shape[0])])
-        labels = resize_and_downsample(labels.squeeze(0), (512, 128), (128, 128))
+        inputs = torch.stack([resize_and_downsample(inputs[i, :, :], (512, 128), (self.resolution, self.resolution)) for i in range(inputs.shape[0])])
+        labels = resize_and_downsample(labels.squeeze(0), (512, 128), (self.resolution, self.resolution))
 
         # Normalising
-        inputs = (inputs - temp_values_t_in.min())/(temp_values_t_in.max() - temp_values_t_in.min())
-        # inputs = (inputs - temp_values_t0.mean())/temp_values_t0.std()
-        labels = (labels - temp_values_t_out.min())/(temp_values_t_out.max() - temp_values_t_out.min())
-        # labels = (labels - temp_values_t300.mean())/temp_values_t300.std()
+        if self.normalize:
+            min_val_in = temp_values_t_in.min()
+            max_val_in = temp_values_t_in.max()
+            min_val_out = temp_values_t_out.min()
+            max_val_out = temp_values_t_out.max()
+            # inputs[-1, :, :] = (inputs[-1, :, :] - min_val_in)/(max_val_in - min_val_in)
+            # TODO: This is wrong!
+            inputs = (inputs - min_val_in)/(max_val_in - min_val_in)
+            # inputs = (inputs - temp_values_t0.mean())/temp_values_t0.std()
+            labels = (labels - min_val_out)/(max_val_out - min_val_out)
+            # labels = (labels - temp_values_t300.mean())/temp_values_t300.std()
 
         if self.model_type == "FNO":
             inputs = inputs.permute(1, 2, 0)
@@ -285,7 +324,7 @@ class StrakaBubblePlottingDataset(Dataset):
     
     
 class StrakaBubble:
-    def __init__(self, network_properties, device, batch_size, training_samples, in_dist = True, model_type="FNO", all_dt=True, t_in=0, t_out=900, dt=60):
+    def __init__(self, network_properties, device, batch_size, training_samples, model_type, in_dist = True, all_dt=True, t_in=0, t_out=900, dt=60):
         
         if model_type == "FNO":
         

Plot.py
@@ -7,7 +7,18 @@ from random import randint
 import h5py
 import pickle
 
-from Dataloaders import StrakaBubble, StrakaBubbleDataset, StrakaBubblePlottingDataset
+from Dataloaders import StrakaBubble, StrakaBubbleDataset, StrakaBubblePlottingDataset, normalize_data
+
+min_data = -28.0
+max_data = 0.0
+min_x = 0.0
+max_x = 25575.0
+min_z = 0.0
+max_z = 6400.0
+min_viscosity = 0.029050784477663294
+max_viscosity = 74.91834790806836
+min_diffusivity = 0.043989764422611155
+max_diffusivity = 74.8587964361266
 
 
 def plot_samples(data_loader, model, n, device, model_type, t_in, t_out, dt, do_fft, cmap='coolwarm', autoreg=True):
@@ -15,8 +26,8 @@ def plot_samples(data_loader, model, n, device, model_type, t_in, t_out, dt, do_
     assert t_in < t_out
     with torch.no_grad():
         for i, batch in enumerate(data_loader):
-            for inputs, outputs in batch:
-                if i == n:
+            if i == n:
+                for inputs, outputs in batch:
                     inputs, outputs = inputs.to(device), outputs.to(device)
                     
                     if model_type == "FNO":
@@ -28,6 +39,27 @@ def plot_samples(data_loader, model, n, device, model_type, t_in, t_out, dt, do_
                     z_coords = inputs[0, 1, :, :].cpu().numpy().flatten()  # Assuming second channel is y-coordinates
                     temp_values_t_in = inputs[0, 4, :, :].cpu().numpy().flatten()  # Assuming fifth channel is temperature
                     temp_values_t_out = outputs[0, 0, :, :].cpu().numpy().flatten()  # Assuming outputs are correctly squeezed
+                    
+                    # Normalizing
+#                     inputs = (inputs + 28)/28
+#                     outputs = (outputs + 28)/28
+
+                    # For new models:
+                    inputs[0, 0, :, :] = (inputs[0, 0, :, :] - min_x)/(max_x - min_x)
+                    inputs[0, 1, :, :] = (inputs[0, 1, :, :] - min_z)/(max_z - min_z)
+                    inputs[0, 2, :, :] = (inputs[0, 2, :, :] - min_viscosity)/(max_viscosity - min_viscosity)
+                    inputs[0, 3, :, :] = (inputs[0, 3, :, :] - min_diffusivity)/(max_diffusivity - min_diffusivity)
+                    inputs[0, 4, :, :] = (inputs[0, 4, :, :] - min_data)/(max_data - min_data)
+                    outputs = (outputs - min_data)/(max_data - min_data)
+                    
+                    # For old models:
+                    # min_val_in = temp_values_t_in.min()
+                    # max_val_in = temp_values_t_in.max()
+                    # min_val_out = temp_values_t_out.min()
+                    # max_val_out = temp_values_t_out.max()
+                    # inputs = (inputs - min_val_in)/(max_val_in - min_val_in)
+                    # outputs = (outputs - min_val_out)/(max_val_out - min_val_out)
+            
                     
                     if autoreg:
                         inputs_running = inputs.clone()
@@ -55,6 +87,13 @@ def plot_samples(data_loader, model, n, device, model_type, t_in, t_out, dt, do_
 
                     predictions = predictions.squeeze(1)
                     predictions = predictions.cpu().numpy().flatten()
+                    
+                    # Scale back the data to original scale
+                    # predictions = predictions * 28 - 28
+                    predictions = predictions * (max_data - min_data) + min_data
+                    # For old models:
+                    # predictions = predictions * (max_val_out - min_val_out) + min_val_out
+
 
                     fig, axes = plt.subplots(1, 3, figsize=(26, 8), sharex=True, sharey=True)
                     
@@ -93,7 +132,7 @@ def plot_samples(data_loader, model, n, device, model_type, t_in, t_out, dt, do_
 
                         # Prediction temperature distribution
                         sc2 = axes[1].imshow(predictions_fft, cmap='hot', interpolation='nearest', extent=[-nx//2, nx//2, -nz//2, nz//2])
-                        axes[1].set_title("FFT of Predicted Temperatures")
+                        axes[1].set_title("FFT of Predicted Temperatures " + model_type)
                         axes[1].set_xlabel('k_x')
                         axes[1].set_ylabel('k_z')
                         fig.colorbar(sc2, ax=axes[1])
@@ -120,7 +159,7 @@ def plot_samples(data_loader, model, n, device, model_type, t_in, t_out, dt, do_
 
                         # Prediction temperature distribution
                         sc2 = axes[1].scatter(x_coords, z_coords, c=predictions, cmap=cmap, s=13)
-                        axes[1].set_title("Predicted Temperatures")
+                        axes[1].set_title("Predicted Temperatures " + model_type)
                         axes[1].set_xlabel('X coordinate')
                         axes[1].set_ylabel('Z coordinate')
                         fig.colorbar(sc2, ax=axes[1])
@@ -136,25 +175,27 @@ def plot_samples(data_loader, model, n, device, model_type, t_in, t_out, dt, do_
                         plt.show()
                         plt.savefig('test_output_plot.png')
                     break
+                print("Finished plotting batch", n)
                 break
 
                     
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# model_path = 'TrainedModels/FNO_straka_bubble_dt_60/model.pkl'
-model_path = 'TrainedModels/CNO_straka_bubble_0_to_600/model.pkl'
+model_path = 'TrainedModels/CNO_straka_bubble_0_to_900_normalized_everywhere/model.pkl'
+# model_path = 'TrainedModels/FNO_straka_bubble_dt_60_normalized_everywhere/model.pkl'
 model_type = model_path.split('TrainedModels/')[1][:3]
 model = torch.load(model_path, map_location=torch.device(device))
 model.eval()
 autoreg = True
 t_in=0
-t_out=600
-dt=600
-do_fft = True
+t_out=900
+dt=900
+do_fft = False
+
 
 if autoreg:
-    test_loader = DataLoader(StrakaBubblePlottingDataset(which="training", training_samples=128, model_type=model_type, t_in=t_in, t_out=t_out), batch_size=1, shuffle=False)
+    test_loader = DataLoader(StrakaBubblePlottingDataset(which="test", training_samples=128, model_type=model_type, t_in=t_in, t_out=t_out, normalize=False), batch_size=1, shuffle=False)
 else:
-    test_loader = DataLoader(StrakaBubbleDataset(which="test", training_samples=128, model_type=model_type, dt=dt), batch_size=1, shuffle=False)
+    test_loader = DataLoader(StrakaBubbleDataset(which="test", training_samples=128, model_type=model_type, dt=dt, normalize=False), batch_size=1, shuffle=False)
 
-plot_samples(test_loader, model, n=2, device=device, model_type=model_type, t_in=t_in, t_out=t_out, dt=dt, do_fft=do_fft, cmap='coolwarm', autoreg=autoreg)
+plot_samples(test_loader, model, n=0, device=device, model_type=model_type, t_in=t_in, t_out=t_out, dt=dt, do_fft=do_fft, cmap='coolwarm', autoreg=autoreg)

TrainCNO.py
@@ -2,6 +2,7 @@ import copy
 import json
 import os
 import sys
+import csv
 
 import pandas as pd
 import xarray as xr
@@ -10,9 +11,18 @@ from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 
-from Problems.CNOBenchmarks import Darcy, Airfoil, DiscContTranslation, ContTranslation, AllenCahn, SinFrequency, WaveEquation, ShearLayer, StrakaBubble
+from Problems.CNOBenchmarks import Darcy, Airfoil, DiscContTranslation, ContTranslation, AllenCahn, SinFrequency, WaveEquation, ShearLayer
+from Dataloaders import StrakaBubble
+from Neuralop_Losses import H1Loss
 
 print(sys.argv)
+
+# CNO Strakabubble training parameters:
+
+all_dt = True
+t_in=0
+t_out=900 
+dt=60
 
 
 if len(sys.argv) == 2:
@@ -24,24 +34,24 @@ if len(sys.argv) == 2:
         "scheduler_step": 10,
         "scheduler_gamma": 0.98,
         "epochs": 300,
-        "batch_size": 16,
-        "exp": 1,                # Do we use L1 or L2 errors? Default: L1
-        "training_samples": 400  # How many training samples?
+        "batch_size": 32,
+        "exp": 2.1,                # Do we use L1 or L2 errors? Default: L1. L1:1, L2:2, H1:2.1
+        "training_samples": 1024-256  # How many training samples?
     }
     model_architecture_ = {
         
         #Parameters to be chosen with model selection:
         # "N_layers": 3,            # Number of (D) & (U) blocks 
-        "N_layers": 7,            # Number of (D) & (U) blocks 
+        "N_layers": 3,            # Number of (D) & (U) blocks 
         "channel_multiplier": 32, # Parameter d_e (how the number of channels changes)
         # "channel_multiplier": 64, # Parameter d_e (how the number of channels changes)
         # "N_res": 4,               # Number of (R) blocks in the middle networs.
-        "N_res": 3,               # Number of (R) blocks in the middle networks.
-        "N_res_neck" : 4,         # Number of (R) blocks in the BN
+        "N_res": 4,               # Number of (R) blocks in the middle networks.
+        "N_res_neck" : 6,         # Number of (R) blocks in the BN
         
         #Other parameters:
         # "in_size": 64,            # Resolution of the computational grid
-        "in_size": 128,            # Resolution of the computational grid
+        "in_size": 256,            # Resolution of the computational grid TODO: change 256
         "retrain": 4,             # Random seed
         "kernel_size": 3,         # Kernel size.
         "FourierF": 0,            # Number of Fourier Features in the input channels. Default is 0.
@@ -71,7 +81,7 @@ if len(sys.argv) == 2:
     #which_example = "shear_layer"
 
     # Save the models here:
-    folder = "TrainedModels/"+"CNO_"+which_example+"_0_to_900_deep"
+    folder = "TrainedModels/"+"CNO_"+which_example+"_dt_60_normalized_everywhere"
         
 else:
     
@@ -120,7 +130,7 @@ elif which_example == "airfoil":
 elif which_example == "darcy":
     example = Darcy(model_architecture_, device, batch_size, training_samples)
 elif which_example == "straka_bubble":
-    example = StrakaBubble(model_architecture_, device, batch_size, training_samples)
+    example = StrakaBubble(model_architecture_, device, batch_size, training_samples, model_type="CNO", all_dt=all_dt, t_in=t_in, t_out=t_out, dt=dt)
 else:
     raise ValueError()
     
@@ -138,6 +148,8 @@ if p == 1:
     loss = torch.nn.L1Loss()
 elif p == 2:
     loss = torch.nn.MSELoss()
+elif p == 2.1:
+    loss = H1Loss(d=2, reduce_dims=(0,1))
     
 best_model_testing_error = 1000 #Save the model once it has less than 1000% relative L1 error
 patience = int(0.2 * epochs)    # Early stopping parameter
@@ -149,6 +161,14 @@ if str(device) == 'cpu':
     print("WE SUGGEST YOU TO RUN THE CODE ON A GPU!")
     print("------------------------------------------")
     print(" ")
+
+    
+# Initialize CSV file and writer
+validation_tracking_path = os.path.join(folder, 'validation_tracking.csv')
+with open(validation_tracking_path, mode='w', newline='') as file:
+    validation_writer = csv.writer(file)
+    # Writing the headers of CSV file
+    validation_writer.writerow(['Epoch', 'Training Loss', 'Training Relative Error', 'Validation Relative Error'])
 
 
 for epoch in range(epochs):
@@ -231,6 +251,11 @@ for epoch in range(epochs):
             file.write("Best Testing Error: " + str(best_model_testing_error) + "\n")
             file.write("Current Epoch: " + str(epoch) + "\n")
             file.write("Params: " + str(n_params) + "\n")
+        
+        with open(validation_tracking_path, mode='a', newline='') as file:
+            validation_writer = csv.writer(file)
+            validation_writer.writerow([epoch, train_mse, train_relative_l2, test_relative_l2])
+        
         scheduler.step()
 
     if counter>patience:

TrainFNO.py
@@ -8,7 +8,16 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
-from Problems.FNOBenchmarks import Darcy, Airfoil, DiscContTranslation, ContTranslation, AllenCahn, SinFrequency, WaveEquation, ShearLayer, StrakaBubble
+from Problems.FNOBenchmarks import Darcy, Airfoil, DiscContTranslation, ContTranslation, AllenCahn, SinFrequency, WaveEquation, ShearLayer
+from Dataloaders import StrakaBubble
+
+# FNO Strakabubble training parameters:
+
+all_dt = True
+t_in=0
+t_out=900 
+dt=60
+
 
 if len(sys.argv) == 2:
 
@@ -18,9 +27,9 @@ if len(sys.argv) == 2:
         "scheduler_step": 0.97,
         "scheduler_gamma": 10,
         "epochs": 300,
-        "batch_size": 16,
+        "batch_size": 32,
         "exp": 1,
-        "training_samples": 256,
+        "training_samples": 1000-256,
     }
     fno_architecture_ = {
         "width": 32,
@@ -47,7 +56,7 @@ if len(sys.argv) == 2:
     #which_example = "shear_layer"
 
     # Save the models here:
-    folder = "TrainedModels/"+"FNO_"+which_example+"_test_dt"
+    folder = "TrainedModels/"+"FNO_"+which_example+"_dt_60_normalized_everywhere"
 
 else:
     folder = sys.argv[1]
@@ -89,7 +98,7 @@ elif which_example == "shear_layer_smooth":
 elif which_example == "darcy":
     example = Darcy(fno_architecture_, device, batch_size,training_samples)
 elif which_example == "straka_bubble":
-    example = StrakaBubble(fno_architecture_, device, batch_size, training_samples)
+    example = StrakaBubble(fno_architecture_, device, batch_size, training_samples, model_type="FNO", all_dt=all_dt, t_in=t_in, t_out=t_out, dt=dt)
 else:
     raise ValueError("the variable which_example has to be one between darcy")
 